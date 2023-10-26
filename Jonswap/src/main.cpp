7fea7aa9d3b1707f9fff5f9327c0b9bce0d3ab6c
Xiaayu <zengxiayu0027@gmail.com>
2023-10-26 13:56:04 -0400

    update output

--- Jonswap/src/main.cpp
+++ Jonswap/src/main.cpp
@@ -1,29 +1,33 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
 #include "Jonswap.hpp"
-#include <matplot/matplot.h>
+#include <fstream>
 
-using namespace matplot;
 
 int main(){
     Jonswap jonswap(150000, 30, 300, 0.5);
-    if(!jonswap.calLIMITED_ETA(300, 0.5, 2.0)){std::cout<<"Failed!"<<std::endl;}
+    jonswap.calLIMITED_ETA(300, 0.5, 2.0);
 
     const std::vector<float>& time = jonswap.getTIME();
     const std::vector<float>& speed = jonswap.getSPEED();
     const std::vector<float>& waveheight = jonswap.getETA();
-    std::cout << waveheight[0] << " " << speed[0] << std::endl;
 
-    figure(1);
-    plot(time, speed);
-    hold(on);
-    plot(time, waveheight)->color({0.f, 0.7f, 0.9f});
-    title("Jonswap Wave in Time Series");
-    xlabel("Time(s)");
-    ylabel("Wave Height(m)");
-    grid(on);
-    show();
+    std::ofstream csvFile("output.csv");
+    if(!csvFile.is_open()){
+        std::cerr<<"Unable to open csv file"<<std::endl;
+        return 1;
+    }
+
+    for (size_t i=0; i<time.size(); i++){
+        csvFile << time[i];
+        csvFile << ",";
+        csvFile << speed[i];
+        csvFile << ",";
+        csvFile << waveheight[i];
+        csvFile << "\n";
+    }
+
+    csvFile.close();
 
     return 0;
 }
